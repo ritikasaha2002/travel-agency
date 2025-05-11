6d4aee6e9ebc9b0f42d149e4e0d0ae288a2779c1
ritikasaha2002 <rsaha3393@gmail.com>
2025-05-11 11:28:36 +0530

    changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+export default home
